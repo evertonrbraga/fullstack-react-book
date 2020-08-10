5c4cb0dba98ccb533794ee417a8517ae16d504c8
Everton Braga <evertonr.braga@gmail.com>
2020-08-10 14:02:06 -0300

    tracking app initial structure

--- time_tracking_app/public/js/app.js
+++ time_tracking_app/public/js/app.js
@@ -1,1 +1,79 @@
+class TimersDashboard extends React.Component {
+  render() {
+    return (
+      <div className="ui three centered grid">
+        <div className="column">
+          <EditableTimerList />
+          <ToggleableTimerForm isOpen={true} />
+        </div>
+      </div>
+    );
+  }
+}
 
+class EditableTimerList extends React.Component {
+  render() {
+    return (
+      <div id="timers">
+        <EditableTimer
+          title="Learn React"
+          project="Web Domination"
+          elapsed="8986300"
+          runningSince={null}
+          editFormOpen={false}
+        />
+        <EditableTimer
+          title="Learn extreme ironing"
+          project="World Domination"
+          elapsed="3890985"
+          runningSince={null}
+          editFormOpen={true}
+        />
+      </div>
+    );
+  }
+}
+
+class EditableTimer extends React.Component {
+  render() {
+    if (this.props.editFormOpen) {
+      return (
+        <TimerForm title={this.props.title} project={this.props.project} />
+      );
+    } else {
+      return (
+        <Timer
+          title={this.props.title}
+          project={this.props.project}
+          elapsed={this.props.elapsed}
+          runningSince={this.props.runningSince}
+        />
+      );
+    }
+  }
+}
+
+class TimerForm extends React.Component {
+  render() {
+    const submitText = this.props.title ? "Update" : "Create";
+    return (
+      <div className="ui centered card">
+        <div className="content">
+          <div className="ui form">
+            <div className="field">
+              <label>Title</label>
+              <input type="text" defaultValue={this.props.title} />
+            </div>
+            <div className="field">
+              <label>Project</label>
+              <input type="text" defaultValue={this.props.project} />
+            </div>
+            <div className="ui two bottom attached buttons">
+              <div className="ui basic blue button"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
