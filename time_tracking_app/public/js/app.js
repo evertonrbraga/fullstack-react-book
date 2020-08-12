cfcee6a14264dc199dc2c8adb2f92e8d5d2c5a06
Everton Braga <evertonr.braga@gmail.com>
2020-08-12 15:24:51 -0300

    tracking app components

--- time_tracking_app/public/js/app.js
+++ time_tracking_app/public/js/app.js
@@ -94,3 +94,28 @@ class ToggleableTimerForm extends React.Component {
     }
   }
 }
+
+class Timer extends React.Component {
+  render() {
+    const elapsedString = helpers.renderElapsedString(this.props.elapsed);
+    return (
+      <div className="ui centered card">
+        <div className="content">
+          <div className="header">{this.props.title}</div>
+          <div className="meta">{this.props.project}</div>
+          <div className="center aligned description">
+            <h2>{elapsedString}</h2>
+          </div>
+          <div className="extra content">
+            <span className="right floated trash icon">
+              <i className="trash icon" />
+            </span>
+          </div>
+        </div>
+        <div className="ui bottom attached blue basic button">Start</div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<TimersDashboard />, document.getElementById("content"));
